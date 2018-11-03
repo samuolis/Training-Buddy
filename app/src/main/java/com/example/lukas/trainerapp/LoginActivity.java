f60930d76b9dc32b4e2f49d5dcf7db51771f3577
Lukas Samuolis <samuolis.lu@gmail.com>
2018-11-03 17:32:34 +0200

    TA-05
    backup

--- app/src/main/java/com/example/lukas/trainerapp/LoginActivity.java
+++ app/src/main/java/com/example/lukas/trainerapp/LoginActivity.java
@@ -22,11 +22,14 @@ import com.facebook.accountkit.AccountKitLoginResult;
 import com.facebook.accountkit.ui.AccountKitActivity;
 import com.facebook.accountkit.ui.AccountKitConfiguration;
 import com.facebook.accountkit.ui.LoginType;
+import com.facebook.login.LoginFragment;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 import java.util.Arrays;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.room.Room;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,24 +39,6 @@ import butterknife.ButterKnife;
  */
 public class LoginActivity extends AppCompatActivity{
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
-    private static final int APP_REQUEST_CODE = 10;
-
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-
-    private static final String EMAIL = "email";
-
-    @BindView(R.id.login_email) ConstraintLayout loginViaEmail;
-    @BindView(R.id.login_phone) ConstraintLayout loginViaPhone;
-    @BindView(R.id.facebook_login_button) LoginButton facebookLoginButton;
-    @BindView(R.id.login_progress) ProgressBar mProgressView;
-    @BindView(R.id.progress_bar_background) View mProgressBarBackground;
-    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,125 +46,15 @@ public class LoginActivity extends AppCompatActivity{
         getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
-        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
-                AppDatabase.class, "login_entity").build();
-        ButterKnife.bind(this); // bind butterknife after
-        callbackManager = CallbackManager.Factory.create();
-        AccessToken accessToken = AccountKit.getCurrentAccessToken();
-        com.facebook.AccessToken fbToken = com.facebook.AccessToken.getCurrentAccessToken();
-        Toast.makeText(this, "fb token "+fbToken, Toast.LENGTH_LONG).show();
-        if (accessToken != null || fbToken !=null){
-            launchMainActivity();
-        }
-
-
-        loginViaEmail.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showProgressBar();
-                onEmailLogin();
-            }
-        });
-
-        loginViaPhone.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showProgressBar();
-                onPhoneLogin();
-            }
-        });
-
-        facebookLoginButton.setReadPermissions(Arrays.asList(EMAIL));
-        // If you are using in a fragment, call loginButton.setFragment(this);
-
-        facebookLoginButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mProgressBarBackground.setVisibility(View.VISIBLE);
-            }
-        });
-
-        // Callback registration
-        facebookLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                launchMainActivity();
-            }
-
-            @Override
-            public void onCancel() {
-                mProgressBarBackground.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-                mProgressBarBackground.setVisibility(View.GONE);
-                String toastMessage = error.getMessage();
-                Toast.makeText(LoginActivity.this,toastMessage, Toast.LENGTH_LONG).show();
-            }
-        });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        // Begin the transaction
+        fragmentManager.beginTransaction()
+                .add(R.id.fragment_frame, new LoginFragment())
+                .commit();
+        // Replace the contents of the container with the new fragment
+        //ft.replace(R.id.fragment_frame, new LoginFragment());
     }
 
-    private void launchMainActivity(){
-        Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(myIntent);
-        finish();
-    }
-
-    @Override
-    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == APP_REQUEST_CODE){
-            AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
-            if (loginResult.getError() != null){
-                String toastMessage = loginResult.getError().getErrorType().getMessage();
-                Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show();
-            }else if (loginResult.getAccessToken() != null){
-                launchMainActivity();
-            }
-        }
-        if (resultCode == 0) {
-            hideProgressBar();
-        }
-    }
-
-    public void onLogin(final LoginType loginType){
-        final Intent intent = new Intent(this, AccountKitActivity.class);
-
-        //configure login type and resource type
-        AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
-                new AccountKitConfiguration.AccountKitConfigurationBuilder(
-                        loginType,
-                        AccountKitActivity.ResponseType.TOKEN);
-        final AccountKitConfiguration configuration = configurationBuilder.build();
-        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, configuration);
-        startActivityForResult(intent,APP_REQUEST_CODE);
-
-    }
-
-    public void onPhoneLogin(){
-        onLogin(LoginType.PHONE);
-    }
-
-    public void onEmailLogin(){
-        onLogin(LoginType.EMAIL);
-    }
-
-
-    private void showProgressBar()
-    {
-        mProgressView.setVisibility(View.VISIBLE);
-        mProgressBarBackground.setVisibility(View.VISIBLE);
-    }
-
-    private void hideProgressBar()
-    {
-        mProgressView.setVisibility(View.GONE);
-        mProgressBarBackground.setVisibility(View.GONE);
-    }
 
 }
 
